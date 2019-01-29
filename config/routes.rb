e945b8eeb8d488088d5d95ab8dbc12fbb7ee8d02
dereksmith83 <mdsmith1983@gmail.com>
2019-01-28 21:02:58 -0600

    added favorites restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
     post"/users" => "users#create"
 
     post "/sessions" => "sessions#create"
+
+    get"/favorites" => "favorites#index"
+    post"/favorites" => "favorites#create"
+    get"/favorites/:id" => "favorites#show"
+    patch"/favorites/:id" => "favorites#update"
+    delete"/favorites/:id" => "favorites#destroy"
   end
 end
